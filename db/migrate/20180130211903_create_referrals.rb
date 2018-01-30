543bd45726469ec7515e3eea1946daed3c878384
dov stokar <dov.stokar@gmail.com>
2018-01-30 16:22:47 -0500

    Add referral migration and association

---
+++ db/migrate/20180130211903_create_referrals.rb
@@ -0,0 +1,14 @@
+class CreateReferrals < ActiveRecord::Migration[5.1]
+  def change
+    create_table :referrals do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :ssn
+      t.string :medicare_id
+      t.string :phone_number
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180130160152) do
+ActiveRecord::Schema.define(version: 20180130211903) do
+
+  create_table "referrals", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "ssn"
+    t.string "medicare_id"
+    t.string "phone_number"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_referrals_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
