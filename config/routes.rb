787b717fd3e59035779ce4520383827cab6e4cea
dov stokar <dov.stokar@gmail.com>
2018-03-05 10:02:51 -0500

    Initial azure deployment commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'user/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'referrals#index'
   resources :users, only: [:index, :show]
   resources :referrals, except: [:destroy] do
     resources :notes, except: [:destroy]
