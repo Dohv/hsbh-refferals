171e7a9853ee005c9c5e872bbf204b7c80e4a5c9
dov stokar <dov.stokar@gmail.com>
2018-05-31 10:48:09 -0400

    updated verified data type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180511144124) do
+ActiveRecord::Schema.define(version: 20180529162551) do
 
   create_table "companies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
